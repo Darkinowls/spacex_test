35075b70e7c380ccf34690d41cc7fbe739117336
Denys Chernousov <blackInOwls@gmail.com>
2023-06-17 17:37:52 +0300

    Do I need text theme?

--- lib/core/app_theme.dart
+++ lib/core/app_theme.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 
 final appTheme = ThemeData(
-  brightness: Brightness.dark,
-  scaffoldBackgroundColor: Colors.black,
-  progressIndicatorTheme: const ProgressIndicatorThemeData(
-    color: Color.fromRGBO(186, 252, 84, 1)
-  ),
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: Colors.black,
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+        color: Color.fromRGBO(186, 252, 84, 1)),
+    // textTheme: const TextTheme(
+    //   bodyLarge: TextStyle(fontSize: 24),
+    //   titleLarge: TextStyle(fontSize: 18),
+    //   titleMedium:
+    //       TextStyle(fontSize: 16, color: Color.fromRGBO(186, 252, 84, 1)),
+    //   titleSmall: TextStyle(fontSize: 14, color: Colors.grey),
+    // )
 );
