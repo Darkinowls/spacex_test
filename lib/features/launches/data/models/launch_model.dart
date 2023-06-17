c9c10a630a6d2c2f96d7f41777f9d37c79d7df7d
Denys Chernousov <blackInOwls@gmail.com>
2023-06-17 17:54:39 +0300

    Deleted data I don't need from models

--- lib/features/launches/data/models/launch_model.dart
+++ lib/features/launches/data/models/launch_model.dart
@@ -1,59 +1,24 @@
 class LaunchModel {
-  final num flightNumber;
   final String missionName;
-  final List<dynamic> missionId;
-  final bool upcoming;
-  final String launchYear;
-  final num launchDateUnix;
-  final DateTime launchDateUtc;
   final DateTime launchDateLocal;
-  final bool isTentative;
-  final String tentativeMaxPrecision;
-  final bool tbd;
   final LaunchSite launchSite;
   final String? wikiUrl;
 
   LaunchModel(
-      {required this.flightNumber,
+      {
       required this.missionName,
-      required this.missionId,
-      required this.upcoming,
-      required this.launchYear,
-      required this.launchDateUnix,
-      required this.launchDateUtc,
       required this.launchDateLocal,
-      required this.isTentative,
-      required this.tentativeMaxPrecision,
-      required this.tbd,
       required this.launchSite, this.wikiUrl});
 
   factory LaunchModel.fromJson(Map<String, dynamic> json) => LaunchModel(
-      flightNumber: json["flight_number"],
       missionName: json["mission_name"],
-      missionId: List<dynamic>.from(json["mission_id"].map((x) => x)),
-      upcoming: json["upcoming"],
-      launchYear: json["launch_year"],
-      launchDateUnix: json["launch_date_unix"],
-      launchDateUtc: DateTime.parse(json["launch_date_utc"]),
       launchDateLocal: DateTime.parse(json["launch_date_local"]),
-      isTentative: json["is_tentative"],
-      tentativeMaxPrecision: json["tentative_max_precision"],
-      tbd: json["tbd"],
       launchSite: LaunchSite.fromJson(json["launch_site"]),
       wikiUrl: json["links"]["wikipedia"]);
 
   Map<String, dynamic> toJson() => {
-        "flight_number": flightNumber,
         "mission_name": missionName,
-        "mission_id": List<dynamic>.from(missionId.map((x) => x)),
-        "upcoming": upcoming,
-        "launch_year": launchYear,
-        "launch_date_unix": launchDateUnix,
-        "launch_date_utc": launchDateUtc.toIso8601String(),
         "launch_date_local": launchDateLocal.toIso8601String(),
-        "is_tentative": isTentative,
-        "tentative_max_precision": tentativeMaxPrecision,
-        "tbd": tbd,
         "launch_site": launchSite.toJson(),
       };
 }
--- lib/features/rockets/data/models/rocket_model.dart
+++ lib/features/rockets/data/models/rocket_model.dart
@@ -1,72 +1,19 @@
 class RocketModel {
-  final num id;
-  final bool active;
-  final num stages;
-  final num boosters;
-  final num costPerLaunch;
-  final num successRatePct;
-  final DateTime firstFlight;
-  final String country;
-  final String company;
   final List<String> flickrImages;
-  final String wikipedia;
-  final String description;
   final String rocketId;
-  final String rocketName;
-  final String rocketType;
 
   RocketModel({
-    required this.id,
-    required this.active,
-    required this.stages,
-    required this.boosters,
-    required this.costPerLaunch,
-    required this.successRatePct,
-    required this.firstFlight,
-    required this.country,
-    required this.company,
     required this.flickrImages,
-    required this.wikipedia,
-    required this.description,
     required this.rocketId,
-    required this.rocketName,
-    required this.rocketType,
   });
 
   factory RocketModel.fromJson(Map<String, dynamic> json) => RocketModel(
-        id: json["id"],
-        active: json["active"],
-        stages: json["stages"],
-        boosters: json["boosters"],
-        costPerLaunch: json["cost_per_launch"],
-        successRatePct: json["success_rate_pct"],
-        firstFlight: DateTime.parse(json["first_flight"]),
-        country: json["country"],
-        company: json["company"],
         flickrImages: List<String>.from(json["flickr_images"].map((x) => x)),
-        wikipedia: json["wikipedia"],
-        description: json["description"],
         rocketId: json["rocket_id"],
-        rocketName: json["rocket_name"],
-        rocketType: json["rocket_type"],
       );
 
   Map<String, dynamic> toJson() => {
-        "id": id,
-        "active": active,
-        "stages": stages,
-        "boosters": boosters,
-        "cost_per_launch": costPerLaunch,
-        "success_rate_pct": successRatePct,
-        "first_flight":
-            "${firstFlight.year.toString().padLeft(4, '0')}-${firstFlight.month.toString().padLeft(2, '0')}-${firstFlight.day.toString().padLeft(2, '0')}",
-        "country": country,
-        "company": company,
         "flickr_images": List<dynamic>.from(flickrImages.map((x) => x)),
-        "wikipedia": wikipedia,
-        "description": description,
         "rocket_id": rocketId,
-        "rocket_name": rocketName,
-        "rocket_type": rocketType,
       };
 }
